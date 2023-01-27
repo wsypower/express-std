b0546b607caf2a082d423265bd2d906d4eaeac61
weiyafei <2553241022@qq.com>
2023-01-27 23:03:55 +0800

    feat: node-esm

---
+++ packages/node-esm/src/esm.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-01-27 22:38:48
+ * @LastEditTime: 2023-01-27 22:58:20
+ * @LastEditors: wsy
+ */
+import { fileURLToPath } from 'node:url';
+
+
+console.log(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+
+console.log(__filename);
