92bdf2c3fdd29438d6eda3c889d6fa4e110127c9
weiyafei <2553241022@qq.com>
2023-01-31 02:01:07 +0800

    feat: bytes

---
+++ packages/bytes/index.ts
@@ -0,0 +1,20 @@
+import bytes from 'bytes'
+
+console.log(bytes(1024));// => '1KB'
+
+console.log(bytes('1KB')); // => 1024
+
+console.log(bytes(1024 * 1024));// => '1MB'
+console.log(bytes('1MB'));// => 1048576
+
+console.log(bytes(1024 * 1024 * 1024));// => '1GB'
+console.log(bytes('1GB'));// => 1073741824
+
+console.log(bytes(1024 * 1024 * 1024 * 1.75517452, {
+  decimalPlaces: 3,
+  unitSeparator: '*-*',
+  unit: 'kb',
+  thousandsSeparator: "-",
+}));// => '1-840-433.877*-*kb'
+console.log(bytes('1000'));// => 1099511627776
+
