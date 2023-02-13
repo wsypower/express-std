1e25901f262a3277b9aebaa7ead73bd439a12a80
weiyafei <2553241022@qq.com>
2023-02-14 01:15:31 +0800

    feat: append

--- packages/app/src/res.ts
+++ packages/app/src/res.ts
@@ -2,16 +2,15 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-02-07 01:55:53
- * @LastEditTime: 2023-02-14 01:10:45
+ * @LastEditTime: 2023-02-14 01:14:10
  * @LastEditors: wsy
  */
 import express from 'express';
 const Response = express()
 
 Response.get('/', (req, res) => {
-  console.log(res.headersSent);
+  res.set('link', '123')
   res.send('Response Homepage');
-  console.log(res.headersSent);
 })
 
 Response.use((req, res, next) => {
