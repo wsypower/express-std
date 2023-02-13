45bf6e4c0bd54060a21b1808914392512e07a9c3
weiyafei <2553241022@qq.com>
2023-02-14 01:22:20 +0800

    feat: cookie

--- packages/app/src/res.ts
+++ packages/app/src/res.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-02-07 01:55:53
- * @LastEditTime: 2023-02-14 01:14:10
+ * @LastEditTime: 2023-02-14 01:21:51
  * @LastEditors: wsy
  */
 import express from 'express';
@@ -10,6 +10,8 @@ const Response = express()
 
 Response.get('/', (req, res) => {
   res.set('link', '123')
+  res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true })
+  res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
   res.send('Response Homepage');
 })
 
