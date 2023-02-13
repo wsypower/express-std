3952b715ba077a5bc400ab9c38de72b86fe9d0ef
weiyafei <2553241022@qq.com>
2023-02-14 01:28:33 +0800

    feat: get

--- packages/app/src/res.ts
+++ packages/app/src/res.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-02-07 01:55:53
- * @LastEditTime: 2023-02-14 01:21:51
+ * @LastEditTime: 2023-02-14 01:26:47
  * @LastEditors: wsy
  */
 import express from 'express';
@@ -13,6 +13,11 @@ Response.get('/', (req, res) => {
   res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true })
   res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
   res.send('Response Homepage');
+})
+
+Response.get('/get', (req, res) => {
+  console.log(res.get('Content-Type'));
+  res.send('Response Homepage1');
 })
 
 Response.use((req, res, next) => {
